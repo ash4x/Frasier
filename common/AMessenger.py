51f46bcf13e233d3148bc3ff9b5ecc169dd248db
fra-sier <95883699+fra-sier@users.noreply.github.com>
2025-01-05 21:56:52 -0600

    AMessenger.py

---
+++ common/AMessenger.py
@@ -0,0 +1,28 @@
+import threading
+
+class AMessenger():
+    def __init__(self):
+        self.lock = threading.Lock()
+        self.msg = None
+        self.msgPrevious = None
+        return
+
+    def Get(self) -> str:
+        with self.lock:
+            self.msgPrevious = self.msg
+            self.msg = None
+            return self.msgPrevious
+        
+    def GetPreviousMsg(self) -> str:
+        return self.msgPrevious
+    
+    def Lock(self):
+        self.lock.acquire()
+        return
+    
+    def Put(self, msg: str):
+        self.msg = msg if "" != msg.strip() else None
+
+    def Unlock(self):
+        self.lock.release()
+        return
